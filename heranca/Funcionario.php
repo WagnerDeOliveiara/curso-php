c03c9934ec5b10b695f06e3df72323762e7bec88
php 11 <php.11@metralha.cec.ime.usp.br>
2017-02-10 22:28:46 -0200

    salvando

--- heranca/Funcionario.php
+++ heranca/Funcionario.php
@@ -5,5 +5,10 @@ class Funcionario
 
   public $nome;
   public $salario;
+  const PORCENTAGEM_DO_BONUS = 0.05;  //declaração de constante
+
+  public function calculaBonus(){
+    return $this->salario * self::PORCENTAGEM_DO_BONUS + 1000;
+  }
 
 }
--- heranca/Gerente.php
+++ heranca/Gerente.php
@@ -8,4 +8,11 @@ class Gerente extends Funcionario
 
   public $senha;
 
+//override, sobrescrita ou reescrita de metodo(criar função na filha com o mesmo nome da do pai(mãe). quem usar essa classe, chamará essa.)
+public function calculaBonus(){
+
+  //"parent::nomeClasse" serve para chamar o metodo da classe pai(mãe) a partir da filha
+  return parent::calculaBonus() + 2 * $this->salario;
+}
+
 }
